1780ad540e61828247da04bf9a3848c36bfe6cdf
spavlenko <spavlenko@optimalcity.ru>
2023-10-07 14:53:52 +0300

    Vue проект

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Тут надо хэдер</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/ArticlesView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <button type="button" class="btn btn-primary">Primary</button>
+</template>
+<style lang="scss"></style>
