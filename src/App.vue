25c4bb7b9fce750ddf9c533c4f203096293685c1
unknown <steponpc@mail.ru>
2023-10-08 22:18:51 +0300

    articles view, шрифты, общие классы для страниц

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <script setup></script>
 
 <template>
-  <header>
-    <h1>Тут надо хэдер</h1>
-  </header>
-
-  <RouterView />
+  <div class="wrapper">
+    <header>
+      <h1>Тут надо хэдер</h1>
+    </header>
+    <RouterView />
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.view {
+  &__title {
+    font-size: 2rem;
+    font-weight: 800;
+    margin-bottom: 4rem;
+  }
+  &__load-btn {
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    font-weight: 900;
+    border: none;
+    border-bottom: 2px solid black;
+    border-radius: 0;
+    padding-inline: 0;
+    margin-top: 3.5rem;
+  }
+  &__input {
+    border-radius: 0;
+    border: 1px solid #1e1e1e;
+    padding: 1rem 2rem;
+  }
+}
+
+body {
+  font-family: 'Gilroy';
+}
+
+.wrapper {
+  max-width: calc(1150px + 2rem);
+  padding-inline: 1rem;
+  margin-inline: auto;
+}
+</style>
--- src/views/ArticlesView.vue
+++ src/views/ArticlesView.vue
@@ -1,5 +1,79 @@
-<script setup></script>
+<script setup>
+import ArticleCard from '@/components/ArticleCard.vue'
+</script>
 <template>
-  <button type="button" class="btn btn-primary">Primary</button>
+  <div>
+    <h2 class="view__title">Биология</h2>
+    <div class="input-group gap-6">
+      <input
+        type="text"
+        class="form-control view__input"
+        aria-label="Text input with dropdown button"
+        placeholder="Введите запрос"
+      />
+      <button
+        class="btn btn-outline-secondary dropdown-toggle ml-5 view__input"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        По новизне
+      </button>
+      <ul class="dropdown-menu dropdown-menu-end">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <li><hr class="dropdown-divider" /></li>
+        <li><a class="dropdown-item" href="#">Separated link</a></li>
+      </ul>
+    </div>
+
+    <div class="sections">
+      <section>
+        <div class="row">
+          <ArticleCard class="col" />
+          <div class="col d-flex flex-column justify-content-between">
+            <ArticleCard v-for="index in 5" :horizontal="true" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="row">
+          <ArticleCard class="col" v-for="index in 3" />
+        </div>
+      </section>
+
+      <section>
+        <div class="row">
+          <ArticleCard class="col" v-for="index in 2" />
+        </div>
+      </section>
+
+      <section>
+        <div class="row">
+          <div class="col d-flex flex-column justify-content-between">
+            <ArticleCard v-for="index in 5" :horizontal="true" />
+          </div>
+          <ArticleCard class="col" />
+        </div>
+      </section>
+
+      <section>
+        <div class="row">
+          <ArticleCard class="col" v-for="index in 3" />
+        </div>
+      </section>
+    </div>
+
+    <button type="button" class="btn view__load-btn">Загрузить еще</button>
+  </div>
 </template>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.sections {
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 7.875rem;
+}
+</style>
