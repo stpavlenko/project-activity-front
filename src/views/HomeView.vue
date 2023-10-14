b75f1616513fabbeead244f08b6cde3834673d4e
spavlenko <spavlenko@optimalcity.ru>
2023-10-14 17:41:01 +0300

    правки верстка

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,19 @@
 <template>
-  <main class="main">
-    <Banner/>
-    <NewArticles/>
-    <Spheres/>
-  </main>
+  <div class="wrapper">
+    <Banner />
+    <NewArticles />
+    <Spheres />
+  </div>
 </template>
 
-<script>
-  import Banner from '../components/Banner/Banner.vue';
-  import NewArticles from '../components/NewArticles/NewArticles.vue'
-  import Spheres from '../components/Spheres/Spheres.vue'
-
-  export default {
-    name: 'HomeView',
-    components: {Banner ,NewArticles, Spheres}
-  }
+<script setup>
+import Banner from '../components/Banner/Banner.vue'
+import NewArticles from '../components/NewArticles/NewArticles.vue'
+import Spheres from '../components/Spheres/Spheres.vue'
 </script>
 
 <style scoped lang="css">
-@font-face {
-    font-family: 'Gilroy';
-    src: local('Gliroy') url('../../assets/fonts/Gilroy-Extrabold.ttf');
-}
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Gilroy', 'sans-serif';
-}
-
 .main {
   margin-bottom: 220px;
 }
-
 </style>
