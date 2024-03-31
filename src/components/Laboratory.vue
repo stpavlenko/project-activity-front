c00f5a741499aabda460e3204002c53e2e5d4b8a
stpavlenko <steponpc@mail.ru>
2024-03-31 22:40:53 +0300

    laboratories view

--- src/components/Laboratory.vue
+++ src/components/Laboratory.vue
@@ -1,16 +1,25 @@
-<script setup></script>
 <template>
   <RouterLink class="router-link-reset" :to="{ name: 'laboratory' }">
     <div class="laboratory">
-      <span class="laboratory__title">Название</span>
-    </div></RouterLink
-  >
+      <div class="laboratory__img"></div>
+      <span class="laboratory__title">Название лаборатории</span>
+    </div>
+  </RouterLink>
 </template>
 <style lang="scss" scoped>
+@use '@/assets/style/variables' as *;
+
 .laboratory {
-  padding: 100px 60px;
-  min-height: 360px;
-  display: flex;
-  align-items: flex-end;
+  &__img {
+    background-color: $bg-tertiary;
+    height: 360px;
+  }
+
+  &__title {
+    display: block;
+    font-weight: 700;
+    font-size: 1.5rem;
+    margin-top: 1.5rem;
+  }
 }
 </style>
--- src/views/LaboratoriesView.vue
+++ src/views/LaboratoriesView.vue
@@ -10,49 +10,31 @@ import Laboratory from '../components/Laboratory.vue'
         type="search"
         class="form-control border-end-0 view__input"
         aria-label="Text input with dropdown button"
-        placeholder="Найти ученого"
+        placeholder="Найти лабораторию"
       />
       <button class="border-start-0 search-button view__input"></button>
     </div>
     <div class="laboratories">
-      <Laboratory class="laboratories__item laboratories__item--span2" />
-      <Laboratory class="laboratories__item laboratories__item--dark" />
-      <Laboratory class="laboratories__item"/>
-      <Laboratory class="laboratories__item laboratories__item--span2 laboratories__item--dark" />
-      <Laboratory class="laboratories__item laboratories__item--span2 laboratories__item--medium" />
-      <Laboratory class="laboratories__item laboratories__item--medium" />
-      <Laboratory class="laboratories__item"/>
-      <Laboratory class="laboratories__item laboratories__item--span2 laboratories__item--dark" />
-      <Laboratory class="laboratories__item laboratories__item--span2 laboratories__item--dark" />
-      <Laboratory class="laboratories__item laboratories__item--medium" />
-      <Laboratory class="laboratories__item"/>
+      <Laboratory v-for="i in 8" :key="i" class="laboratories__item" />
     </div>
     <button type="button" class="btn view__load-btn">Загрузить еще</button>
   </div>
 </template>
 <style lang="scss" scoped>
+
 .laboratories {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
+  gap: 3rem 2.5rem;
+  grid-template-columns: repeat(2, 1fr);
 
-  &__item {
-    background-color: #d9d9d9;
-    &--span2 {
-      grid-column: span 2;
-    }
-    &--dark {
-      background-color: #999999;
-    }
-    &--medium {
-      background-color: #c2c2c2;
-    }
-  }
+
 }
+
 .wrapper {
   margin-top: 2.75rem;
   margin-bottom: 13.75rem;
 }
+
 h2 {
   margin-top: 2.75rem;
 }
@@ -60,6 +42,7 @@ h2 {
 .search {
   margin-bottom: 2.75rem;
 }
+
 .search-button {
   background: transparent url('../../public/searchIcon.svg') no-repeat center;
   width: 24px;
